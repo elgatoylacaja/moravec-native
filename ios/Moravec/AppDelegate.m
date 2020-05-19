c717a385459b1129c6ebe8c417985e2708717d0c
Lucas Rodriguez <lrodriguez@eryx.co>
2020-05-19 20:18:22 -0300

    Added assets

--- ios/Moravec/AppDelegate.m
+++ ios/Moravec/AppDelegate.m
@@ -10,7 +10,7 @@
 #import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
-#import "SplashScreen.h"
+#import "RNSplashScreen.h"
 
 @implementation AppDelegate
 
@@ -28,7 +28,7 @@
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
-  [SplashScreen show];
+  [RNSplashScreen show];
   return YES;
 }
 
